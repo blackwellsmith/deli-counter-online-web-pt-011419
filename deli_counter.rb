d543bfe8205937d14433674ae8fac1baf0f276e3
blackwell <blackwellsmithiv@gmail.com>
2019-01-21 21:45:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+katz_deli = []
+def line(katz_deli)
+  if katz_deli.length == 0 
+    puts "The line is currently empty."
+  else
+    katz_deli.each do |names|
+    current_line = "The line is currently:"
+    current_line << " #{katz_deli.index(names) + 1}. #{names}" 
+  end
+    puts current_line
+end 
