a647bdfc2c0c5d69e861de9cd27375f459df5e86
blackwell <blackwellsmithiv@gmail.com>
2019-01-21 22:19:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,9 +3,8 @@ def line(array)
   if array.length == 0 
     puts "The line is currently empty."
   else
-    
+    current_line = "The line is currently:"
     array.each do |names|
-    
     current_line << " #{array.index(names) + 1}. #{names}"
   end
   puts current_line 
