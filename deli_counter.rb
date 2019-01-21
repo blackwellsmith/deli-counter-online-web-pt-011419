26112c3c1c4795d16b7cb79ebc2dc70cc1269a96
blackwell <blackwellsmithiv@gmail.com>
2019-01-21 22:19:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,6 +3,7 @@ def line(array)
   if array.length == 0 
     puts "The line is currently empty."
   else
+    
     array.each do |names|
     
     current_line << " #{array.index(names) + 1}. #{names}"
